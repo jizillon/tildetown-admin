a2af7c5c96e47aba1579a3504bb2796bb308b7d2
Nate Smith <vilmibm@github.com>
2019-07-15 20:48:05 -0500

    restore this field to make migrations easier

--- ttadmin/users/models.py
+++ ttadmin/users/models.py
@@ -58,6 +58,7 @@ class Townie(User):
     )
     shell = CharField(max_length=50, default="/bin/bash")
     state = CharField(max_length=20, choices=STATE_CHOICES, default=UNREVIEWED)
+    reviewed = BooleanField(default=False)
     reasons = TextField(blank=True, null=False, default='')
     plans = TextField(blank=True, null=False, default='')
     socials = TextField(blank=True, null=False, default='')
