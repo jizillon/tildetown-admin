9373589c52355f2d20f9b04013ce8e3a774af94b
Nate Smith <vilmibm@github.com>
2019-07-15 20:38:11 -0500

    code for adding a state field to users

--- ttadmin/users/admin.py
+++ ttadmin/users/admin.py
@@ -9,20 +9,27 @@ class PubkeyInline(admin.TabularInline):
     model = Pubkey
     extra = 1
 
-def bulk_review(madmin, req, qs):
+def bulk_accept(madmin, req, qs):
     for townie in qs:
-        townie.reviewed = True
+        townie.state = 'accepted'
         townie.save()
     post_users_to_social(qs)
 
-bulk_review.short_description = 'mark selected townies as reviewed'
+bulk_accept.short_description = 'mark selected townies as accepted'
+
+def bulk_reject(madmin, req, qs):
+    for townie in qs:
+        townie.state = 'rejected'
+        townie.save()
+
+bulk_reject.short_description = 'mark selected townies as rejected'
 
 @admin.register(Townie)
 class TownieAdmin(admin.ModelAdmin):
     inlines = [PubkeyInline]
-    list_display = ('username', 'reviewed', 'email')
-    ordering = ('reviewed',)
+    list_display = ('username', 'state', 'email')
     readonly_fields = ('reasons', 'plans', 'socials')
+    ordering = ('state',)
     exclude = ('first_name', 'last_name', 'password', 'groups', 'user_permissions', 'last_login', 'is_staff', 'is_active', 'is_superuser')
-    actions = (bulk_review,)
+    actions = (bulk_accept, bulk_reject,)
     search_fields = ('username', 'email', 'displayname')
